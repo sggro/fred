ea92b3c50323ffa3de0efdf92ee361e6fceb6f35
vagrant <vagrant@shopware.local>
2021-01-22 14:40:05 +0000

    Add uninstall method to drop columns

--- src/SgFredConfigurator.php
+++ src/SgFredConfigurator.php
@@ -1,9 +1,28 @@
 <?php declare(strict_types=1);
 
-namespace SgFredConfigurator;
+namespace Sg\FredConfigurator;
 
+use Doctrine\DBAL\Connection;
 use Shopware\Core\Framework\Plugin;
+use Shopware\Core\Framework\Plugin\Context\UninstallContext;
 
 class SgFredConfigurator extends Plugin
 {
+    public function uninstall(UninstallContext $context): void
+    {
+        parent::uninstall($context);
+
+        if ($context->keepUserData()) {
+            return;
+        }
+
+        $connection = $this->container->get(Connection::class);
+        $connection->executeUpdate('DROP TABLE IF EXISTS `sg_fred_configuration`');
+        $connection->executeUpdate('DROP TABLE IF EXISTS `sg_fred_flow`');
+
+        $connection->executeUpdate('ALTER TABLE `product` ADD COLUMN `sg_fred_configuration_id`');
+
+    }
+
+
 }
